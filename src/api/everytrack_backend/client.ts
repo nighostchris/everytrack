5e4defa71634a7f9df2de96245b837401669ec94
Chris <chrisliupascal@gmail.com>
2023-09-13 19:49:47 +0800

    Add axios interceptor into everytrack-backend client call to include requestId and possible local storage token

--- src/api/everytrack_backend/client.ts
+++ src/api/everytrack_backend/client.ts
@@ -1,11 +1,18 @@
+import { v4 } from 'uuid';
 import axios from 'axios';
 
 import { env } from '@config';
 
 export const client = axios.create({
   baseURL: env.VITE_NODE_ENV === 'development' ? `${env.VITE_BACKEND_HOST}:${env.VITE_BACKEND_PORT}` : env.VITE_BACKEND_HOST,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-  },
   withCredentials: true,
+});
+
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  config.headers['X-Request-Id'] = v4();
+  return config;
 });
