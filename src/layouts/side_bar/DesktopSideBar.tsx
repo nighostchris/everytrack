cdf8298c8451e068574872f92d019e7bf9485b63
Chris <chrisliupascal@gmail.com>
2024-02-21 18:08:17 +0000

    Redesigning new side bar in desktop mode

--- src/layouts/side_bar/DesktopSideBar.tsx
+++ src/layouts/side_bar/DesktopSideBar.tsx
@@ -2,39 +2,46 @@ import clsx from 'clsx';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import ProfileBar from './ProfileBar';
 import { SIDE_BAR_TABS } from './consts';
 
 const DesktopSideBar: React.FC = () => {
   const { pathname } = useLocation();
 
   return (
-    <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
-      <div className="flex min-h-0 flex-1 flex-col bg-gray-800">
-        <div className="flex flex-1 flex-col overflow-y-auto pb-4 pt-5">
-          <div className="flex flex-shrink-0 items-center px-4">
-            <img className="w-full" src="/logo.svg" alt="Everytrack" />
-          </div>
-          <nav className="mt-5 flex-1 space-y-1 px-2">
-            {SIDE_BAR_TABS.map(({ name, icon: Icon, link }) => (
-              <Link
-                to={link}
-                key={`desktop-side-nav-${name}`}
-                className={clsx(
-                  {
-                    'bg-gray-900 text-white': pathname.includes(link),
-                    'text-gray-300 hover:bg-gray-700 hover:text-white': !pathname.includes(link),
-                  },
-                  'group flex items-center rounded-md px-2 py-2 text-sm font-medium',
-                )}
-              >
-                <Icon className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300" />
-                {name}
-              </Link>
-            ))}
-          </nav>
-        </div>
-        <ProfileBar />
+    <div className="hidden md:fixed md:inset-y-0 md:flex md:w-20 md:flex-col">
+      <div className="flex min-h-0 flex-1 flex-col items-center justify-between bg-gray-800 py-6">
+        <Link to="/">
+          <img className="px-4" src="/favicon.svg" alt="Everytrack" />
+        </Link>
+        <nav className="space-y-1.5">
+          {SIDE_BAR_TABS.map(({ name, icon: Icon, link }) => (
+            <Link
+              to={link}
+              key={`desktop-side-nav-${name}`}
+              className={clsx(
+                {
+                  'bg-gray-900 text-white': pathname.includes(link),
+                  'text-gray-300 hover:bg-gray-700 hover:text-white': !pathname.includes(link),
+                },
+                'group flex items-center rounded-full p-3',
+              )}
+            >
+              <Icon className="h-6 w-6 text-gray-400 group-hover:text-gray-300" />
+            </Link>
+          ))}
+        </nav>
+        <Link
+          to="/settings"
+          className={clsx(
+            {
+              'bg-gray-900': pathname.includes('/settings'),
+              'hover:bg-gray-700': !pathname.includes('/settings'),
+            },
+            'rounded-full p-3',
+          )}
+        >
+          <img className="h-6 w-6" src="https://ionicframework.com/docs/img/demos/avatar.svg" alt="Settings" />
+        </Link>
       </div>
     </div>
   );
