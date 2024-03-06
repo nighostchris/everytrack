316670960e5deec5a114aff9abc623e9e9af5176
Chris <chrisliupascal@gmail.com>
2024-03-06 13:13:55 +0000

    Introducing react-big-calendar to kick start calendar view for future payments page

--- src/pages/payments_page/index.tsx
+++ src/pages/payments_page/index.tsx
@@ -1,16 +1,23 @@
 /* eslint-disable max-len */
 import clsx from 'clsx';
+import dayjs from 'dayjs';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import { useShallow } from 'zustand/react/shallow';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
+import { Calendar, dayjsLocalizer } from 'react-big-calendar';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
 import 'react-toastify/dist/ReactToastify.css';
+import './calendar.css';
 
 import { Root } from '@layouts/root';
-// import { useDisplayCurrency } from '@hooks';
 import { store } from '@features/payments/zustand';
 import { usePaymentsState } from '@features/payments/hooks/use_payments_state';
 import { PaymentsTable, AddNewFuturePaymentModal, DeleteFuturePaymentModal, EditFuturePaymentModal } from '@features/payments/components';
+
+const localizer = dayjsLocalizer(dayjs);
+const DragAndDropCalendar = withDragAndDrop(Calendar);
 
 export const PaymentsPage: React.FC = () => {
   const { openAddNewFuturePaymentModal, openEditFuturePaymentModal, openDeleteFuturePaymentModal } = store(
@@ -36,6 +43,43 @@ export const PaymentsPage: React.FC = () => {
       >
         <h1 className="text-xl font-semibold text-gray-900">Payments</h1>
         <p className="mt-2 text-sm text-gray-700">Monitor your rolling incomes and subscriptions</p>
+        {/* Constructions on going */}
+        <div className="mt-10 min-h-[632px] w-full rounded-lg bg-white pb-8">
+          <DragAndDropCalendar
+            views={['month']}
+            defaultView="month"
+            localizer={localizer}
+            formats={{
+              dateFormat: 'D',
+            }}
+            components={{
+              toolbar: (props) => {
+                console.log(props);
+                return (
+                  <div className="flex flex-row items-center justify-between border-b border-b-gray-200 px-6 py-4">
+                    <h2 className="text-lg font-medium">{dayjs(props.date).format('MMMM YYYY')}</h2>
+                    <div className="flex flex-row items-center space-x-6">
+                      <span className="rounded-lg border border-gray-200 px-3 py-2 text-sm shadow-sm hover:cursor-pointer">Today</span>
+                      <FaArrowLeft
+                        className="h-5 w-5 hover:cursor-pointer"
+                        onClick={() => {
+                          props.onNavigate('PREV');
+                        }}
+                      />
+                      <FaArrowRight
+                        className="h-5 w-5 hover:cursor-pointer"
+                        onClick={() => {
+                          props.onNavigate('NEXT');
+                        }}
+                      />
+                    </div>
+                  </div>
+                );
+              },
+            }}
+          />
+        </div>
+        {/* Constructions on going */}
         <PaymentsTable data={paymentsTableRows} className="!mt-10" />
       </div>
       <ToastContainer />
